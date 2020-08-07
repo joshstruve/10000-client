99822c2ba85cceb7556668aa469741e880b3cda1
Josh Struve <jstruve@gmail.com>
2020-08-07 08:29:45 -0700

    demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, Switch} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home/Home'
+import Login from './routes/Login'
+import Register from './routes/Register'
+import Skills from './routes/Skills'
+import AddSkill from './routes/AddSkill'
+
+import PublicOnlyRoute from './components/Utils/PublicOnlyRoute';
+import PrivateRoute from './components/Utils/PrivateRoute';
+import TokenService from './services/token-service';
+
+export default class App extends Component {
+  state = {
+    loggedInToggle:false,
+  }
+
+  handleLogIn = (history) => {
+    this.setState({loggedInToggle:true})
+
+    history.push('/skills')
+  }
+
+  handleLogout = () => {
+    TokenService.clearAuthToken()
+    this.setState({loggedInToggle:false})
+  }
+
+  render() {
+    return (
+      <main className="App">
+        <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/skills' component={Skills} />
+        <Route path='/addskill' component={AddSkill} />
+        <Route path='/login' component={props => <Login {...props} handleLogIn={this.handleLogIn}/>}/>
+        <Route path='/register' component={props => <Register {...props} handleLogIn={this.handleLogIn}/>}/>
+        </Switch>
+      </main>
+    );
+  }
 }
 
-export default App;
